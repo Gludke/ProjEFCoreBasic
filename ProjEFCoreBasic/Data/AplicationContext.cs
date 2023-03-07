275725f541ab5e2c6795874f93275021f0076888
Guilherme Ludke <guiludke@gmail.com>
2023-03-07 13:37:53 -0300

    logger

--- ProjEFCoreBasic/Data/AplicationContext.cs
+++ ProjEFCoreBasic/Data/AplicationContext.cs
@@ -1,10 +1,15 @@
 ﻿using CursoEFCore.Domain;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace ProjEFCoreBasic.Data
 {
     public class AplicationContext : DbContext
     {
+        //Gera o log das operações no DB. Necessário o package 'Microsoft.Extensions.Logging.Console'
+        //Adiciona o log no console da app
+        private static readonly ILoggerFactory _logger = LoggerFactory.Create(l => l.AddConsole());
+
         //Criando as tabelas do DB
         public DbSet<Pedido> Pedido { get; set; }
         public DbSet<Produto> Produto { get; set; }
@@ -14,6 +19,10 @@ namespace ProjEFCoreBasic.Data
         //Configura o BD da aplicação
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+            //Registra o log das operaçoes do EFCore no nosso logger
+            optionsBuilder.UseLoggerFactory(_logger);
+            //Mostra os dados sensíveis das operações do EFCore
+            optionsBuilder.EnableSensitiveDataLogging();
             optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=DbEFCoreBasic;Trusted_Connection=True;");
         }
 
