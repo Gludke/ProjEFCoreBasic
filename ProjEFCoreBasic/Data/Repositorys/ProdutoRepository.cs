b4b031a40e792e821c8eb06b55a501f60e5c9855
Guilherme Ludke <guiludke@gmail.com>
2023-03-07 14:51:43 -0300

    final

---
+++ ProjEFCoreBasic/Data/Repositorys/ProdutoRepository.cs
@@ -0,0 +1,20 @@
+﻿using CursoEFCore.Domain;
+
+namespace ProjEFCoreBasic.Data.Repositorys
+{
+    public static class ProdutoRepository
+    {
+        public static void InserirProduto()
+        {
+            var produto = new Produto
+            {
+                Descricao = "aaaaaaa"
+            };
+        }
+
+
+
+
+    }
+
+}
